0b1e1d718f0d863fc6e61350c5d06071abb9b187
Jonas Klarstrup <jonas@klarstrup.dk>
2017-10-22 02:43:10 +0200

    be less gracious with arguments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { forEachObjIndexed, pick, equals, curry } from "ramda";
 
-export default curry((propsHandlersMap = {}, oldProps = {}, newProps = {}) =>
+export default curry((propsHandlersMap, oldProps, newProps) =>
   forEachObjIndexed((handler, keysString) => {
     const keys = keysString.split(",");
     if (!equals(pick(keys)(oldProps), pick(keys)(newProps))) {
